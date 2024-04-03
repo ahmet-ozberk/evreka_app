5caf5f3a6dad7fbdbdc2ff7899c58937e2e56699
ahmet-ozberk <ahmet.ozberk.mrs@gmail.com>
2024-04-03 22:24:39 +0300

    Firebase initialized

--- lib/app/init/starter.dart
+++ lib/app/init/starter.dart
@@ -1,8 +1,16 @@
+import 'package:evreka_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
 final class Starter {
   // Private constructor
   Starter._();
 
   static Future<void> start() async {
-    
+    WidgetsFlutterBinding.ensureInitialized();
+
+    // Firebase initialization
+    final currPlatform = DefaultFirebaseOptions.currentPlatform;
+    await Firebase.initializeApp(options: currPlatform);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:evreka_app/app/init/starter.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+void main() async{
+  await Starter.start();
   runApp(const ProviderScope(child: EvrekaApp()));
 }
 
